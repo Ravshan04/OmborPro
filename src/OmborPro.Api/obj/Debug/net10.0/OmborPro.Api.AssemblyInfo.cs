9b90b8384ae3f9d549ea2f7bfae1bc927ba5663a
Acer <rn3925471@gmail.com>
2026-02-09 23:15:47 +0500

    Initial setup

--- src/OmborPro.Api/obj/Debug/net10.0/OmborPro.Api.AssemblyInfo.cs
+++ src/OmborPro.Api/obj/Debug/net10.0/OmborPro.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OmborPro.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69433b47d29ecfd650a40af9f0ff793d03545ea1")]
 [assembly: System.Reflection.AssemblyProductAttribute("OmborPro.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OmborPro.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/OmborPro.Infrastructure/obj/Debug/net10.0/OmborPro.Infrastructure.AssemblyInfo.cs
+++ src/OmborPro.Infrastructure/obj/Debug/net10.0/OmborPro.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OmborPro.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69433b47d29ecfd650a40af9f0ff793d03545ea1")]
 [assembly: System.Reflection.AssemblyProductAttribute("OmborPro.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OmborPro.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
